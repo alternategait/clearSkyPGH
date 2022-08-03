fcc83efa1b73edcfe40e401458b2226432362a78
Brandi <Brandi@Brandis-MacBook-Air.local>
2022-08-03 12:18:15 -0400

    added fetch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const button = document.getElementById('button')
+button.addEventListener('click', showSpace)
+
+
+function showSpace(){
+    fetch ("https://api.nasa.gov/")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+}
